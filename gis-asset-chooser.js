d7a0b50c2f1a79fdfe40a8cd862c990a81b1fe84
longk <kevinlong.dev@gmail.com>
2024-06-05 16:54:52 -0500

    adding inner html and variables

--- gis-asset-chooser.js
+++ gis-asset-chooser.js
@@ -3,29 +3,42 @@ class GISAssetChooserComponent extends HTMLElement {
     super();
   }
   connectedCallback() {
-    const div = document.createElement("div");
-    div.id = "viewDiv";
-    div.style.width = "60%";
-    div.style.height = "60vh";
-    this.appendChild(div);
-    require(["esri/config", "esri/Map", "esri/views/MapView"], function (
-      esriConfig,
-      Map,
-      MapView
-    ) {
-      esriConfig.apiKey =
-        "AAPK1af1e90a1ee2405a912eb235152854062ll-5gN7QQk-TSyXgKTR7HoKrqRAcw7RseJvj4d6jlHhucrqvv-yD6mJFYA5iSO9";
-      const map = new Map({
-        basemap: "arcgis/topographic",
-        // basemap: "streets-navigation-vector",
-      });
-      const view = new MapView({
-        map: map,
-        center: [-90.25, 38.64], // Longitude, latitude
-        zoom: 14, // Zoom level
-        container: this.querySelector("#viewDiv"), // Div element
-      });
-    }.bind(this));
+    try {
+      const title = this.getAttribute("title") || "This is the title";
+      const hint = this.getAttribute("hint") || "This is the hint";
+      const layers = this.getAttribute("layers") || [];
+      const zoom = this.getAttribute("zoom") || 12;
+      const baseMapURL = this.getAttribute("baseMap") || "streets-navigation-vector";
+      const showSearch = this.getAttribute("showSearch") || false;
+      const centerX = this.getAttribute("centerX") || -90.25;
+      const centerY = this.getAttribute("centerY") || 38.64;
+      const allowPoints = this.getAttribute("allowPoints") || false;
+
+      this.innerHTML = `
+        <p>${title}</p>
+        <p>${hint}</p>
+        <div id="viewDiv" style="width: 30%; height: 60vh;"></div>
+      `;
+
+      require(["esri/Map", "esri/views/MapView"], function (
+        Map,
+        MapView
+      ) {
+        const map = new Map({
+          // basemap: "arcgis/topographic",
+          basemap: baseMapURL,
+        });
+        const view = new MapView({
+          map: map,
+          center: [centerX, centerY], // Longitude, latitude
+          zoom: zoom, // Zoom level
+          container: this.querySelector("#viewDiv")
+        });
+      }.bind(this));
+    } catch (e) {
+      console.error(e);
+      document.getElementById("viewDiv").innerHTML = `<p>There was a problem loading the map. Please try again later.</p>`;
+    }
   }
 }
 
