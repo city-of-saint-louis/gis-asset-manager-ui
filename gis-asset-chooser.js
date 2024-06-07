3321f09f3d3026ee5bc4abc01e213b2c7a3b61eb
longk <kevinlong.dev@gmail.com>
2024-06-07 08:42:40 -0500

    commit

--- gis-asset-chooser.js
+++ gis-asset-chooser.js
@@ -8,7 +8,7 @@ class GISAssetChooserComponent extends HTMLElement {
       const hint = this.getAttribute("hint") || "";
       const layers = this.getAttribute("layers") || [];
       const zoom = this.getAttribute("zoom") || 12;
-      const baseMapURL = this.getAttribute("baseMap") || "streets-navigation-vector";
+      const baseMap = this.getAttribute("baseMap") || "streets-vector"; // topo-vector
       const showSearch = this.getAttribute("showSearch") || false;
       const centerX = this.getAttribute("centerX") || -90.25;
       const centerY = this.getAttribute("centerY") || 38.64;
@@ -17,27 +17,45 @@ class GISAssetChooserComponent extends HTMLElement {
       this.innerHTML = `
         <p>${title}</p>
         <p>${hint}</p>
-        <div id="viewDiv" style="width: 40%; height: 35vh;"></div>
+        <div id="viewDiv" style="width: 30%; height: 50vh;"></div>
       `;
 
-      require(["esri/Map", "esri/views/MapView"], function (
+      require([
+        "esri/config",
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/GraphicsLayer",
+        "esri/layers/FeatureLayer",
+        
+      ], function (
+        esriConfig,
         Map,
-        MapView
+        MapView,
+        Graphic,
+        GraphicsLayer,
+        FeatureLayer,
       ) {
+        
+        // esriConfig.apiKey="AAPK1af1e90a1ee2405a912eb235152854062ll-5gN7QQk-TSyXgKTR7HoKrqRAcw7RseJvj4d6jlHhucrqvv-yD6mJFYA5iSO9"
+
         const map = new Map({
-          // basemap: "arcgis/topographic",
-          basemap: baseMapURL,
+          basemap: baseMap,
         });
+
         const view = new MapView({
           map: map,
           center: [centerX, centerY], // Longitude, latitude
           zoom: zoom, // Zoom level
-          container: this.querySelector("#viewDiv")
+          container: this.querySelector("#viewDiv"),
         });
+
+
       }.bind(this));
     } catch (e) {
       console.error(e);
-      document.getElementById("viewDiv").innerHTML = `<p>There was a problem loading the map. Please try again later.</p>`;
+      document.getElementById(
+        "viewDiv"
+      ).innerHTML = `<p>There was a problem loading the map. Please try again later.</p>`;
     }
   }
 }
