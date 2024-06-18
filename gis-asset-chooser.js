84fafe038b479aac58f92b8643096c867afcf496
longk <kevinlong.dev@gmail.com>
2024-06-18 08:47:09 -0500

    comment out console logs that aren't needed

--- gis-asset-chooser.js
+++ gis-asset-chooser.js
@@ -39,8 +39,8 @@ class GISAssetChooserComponent extends HTMLElement {
 document.addEventListener("layerDetailsProvided", (event) => {
   const mapLayer = event.detail;
   mapLayersToAdd.push(mapLayer);
-  console.log("mapLayer", mapLayer);
-  console.log("mapLayersToAdd", mapLayersToAdd);
+  // console.log("mapLayer", mapLayer);
+  // console.log("mapLayersToAdd", mapLayersToAdd);
 });
 
 function initializeMap() {
@@ -96,7 +96,7 @@ function initializeMap() {
           },
         });
         layerToAdd.outFields = ["*"];
-        console.log("layerToAdd", layerToAdd);
+        // console.log("layerToAdd", layerToAdd);
         layerToAdd.popupEnabled = false;
         map.add(layerToAdd);
       });
@@ -112,27 +112,27 @@ function initializeMap() {
 
           if (response.results.length) {
             const graphic = response.results[0].graphic;
-            console.log("Graphic:", graphic);
+            // console.log("Graphic:", graphic);
             // Get the layer info for this graphic
             const layerInfo = response.results[0].layer.portalItem;
             const layerPortalID = layerInfo.id;
-            console.log("Layer's portal ID: " + layerPortalID);
+            // console.log("Layer's portal ID: " + layerPortalID);
 
             if (layerPortalID === "34f817a794c64919affc7ec449677de3") {
               isParcel = true;
-              console.log("isParcel", isParcel);
+              // console.log("isParcel", isParcel);
             }
             if (layerPortalID === "b0a2bf75ab284aba834328a5a8f6e28b") {
               isBike = true;
-              console.log("isBike", isBike);
+              // console.log("isBike", isBike);
             }
             if (layerPortalID === "46bd9d471a184f20a773224f494c45c8") {
               isTree = true;
-              console.log("isTree", isTree);
+              // console.log("isTree", isTree);
             }
             if (layerPortalID === "0da094b7d469485e9cd5172625cf6513") {
               isFood = true;
-              console.log("isFood", isFood);
+              // console.log("isFood", isFood);
             }
             if (isFood) {
               console.log("Food site selected", graphic.attributes);
