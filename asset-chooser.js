2c076c8ac13ef11bbb7347773507ef8a540641c9
longk <kevinlong.dev@gmail.com>
2025-08-28 17:25:38 -0500

    update

--- asset-chooser.js
+++ asset-chooser.js
@@ -1,24 +1,6 @@
 // import { addMapLayer } from "./asset-chooser-helpers";
 // This file holds the logic that provides functionality for the GIS Asset Chooser
 
-// // set variables to hold default values and arrays to hold data for the GIS Asset Chooser
-// const defaultZoom = 12;
-// const defaultCenterX = -90.25;
-// const defaultCenterY = 38.64;
-// const defaultBaseMap = "topo-vector";
-// const defaultShowSearch = true;
-// const mapLayersToAdd = [];
-// const featureLayers = [];
-// const chosenAssets = [];
-// const chosenAssetFormData = [];
-// const allMapLayerIds = [];
-// const layersWithNoSelectionRequired = [];
-// const validLayers = [];
-// let addressMarkerX;
-// let addressMarkerY;
-// let isValid = false;
-// let currentView = null;
-
 // event listener to caputre x,y coordinates from address validation
 document.addEventListener("coordinatesAvailable", (event) => {
   addressMarkerX = event.detail.centerX;
